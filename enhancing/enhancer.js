1c51a3b0f8a927b54108769eefc63cb32a682a9b
taty2010 <shydalys123@gmail.com>
2020-04-30 13:43:41 -0500

    adding test to success and repair

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,48 @@ module.exports = {
   get,
 };
 
-function succeed(item) {
-  return { ...item };
+
+
+//Item Name
+//Item durability is from 0-20
+//Item enhancement is from 0-100
+
+const item1 = {
+  durability: 50,
+  enhancement: 20
 }
 
-function fail(item) {
-  return { ...item };
+const item2 = {
+  durability: 50,
+  enhancement: 15
 }
+
+function succeed(item) {
+  if (item.enhancement < 20 ){
+    return { ...item, enhancement: item.enhancement + 1 } 
+  }
+  // return { ...item };
+  //enhancement increases by 1
+  //if at 20 no change
+  //no change to durability
+}
+
+// function fail(item) {
+//   return { ...item };
+//   //if item *enhancement < 15 decrease *durability by 5
+//   //if item *enhancement >= 15 decrease *durability by 10
+//   //if item *enhancement> 16 decrease *enhancement by 1
+// }
 
 function repair(item) {
-  return { ...item };
+  let newItem = item
+  if (newItem.durability < 100){
+    return { ...newItem, durability: 100 };
+  }
+  //return new item with durability = 100
 }
 
 function get(item) {
   return { ...item };
 }
+
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,36 @@
-const enhancer = require('./enhancer.js');
+const {repair, succeed} = require('./enhancer.js');
 // test away!
+
+const sword = {
+  name: 'Grass Sword',
+  durability: 50,
+  enhancement: 20
+}
+
+const shield = {
+  name: 'Hylian shield',
+  durability: 50,
+  enhancement: 15
+}
+
+describe('enhance item requests', () => {
+  describe('succeed', () => {
+      it('return enhancement + 1 ', () => {
+        expect(succeed(shield).enhancement).toBe(16)
+      })
+      it('Item should have a name', () => {
+        expect(succeed(shield).name).toBe('Hylian shield')
+      })
+  })
+  describe('fail', () => {
+    it.todo('')
+  })
+  describe('repair', () => {
+    it('return durability of 100', () => {
+      expect(repair(sword).durability).toBe(100)
+    })
+  })
+  describe('get', () => {
+    it.todo('')
+  })
+})
